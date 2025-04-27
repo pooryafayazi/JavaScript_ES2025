55c1038a9c54b8f9b695f47b48c138f9ae7b8d9a
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-04-27 20:21:24 +0330

    03-036

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+
+
+// 03-036
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log('I can drive!');
+
+// reserved keywords
+const interface = 'Audio';
+const private = 534;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
