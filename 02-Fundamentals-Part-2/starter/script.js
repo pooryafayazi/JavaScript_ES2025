3ad97d67fb8832c5d2542a43f139258d03da1eb4
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-04-27 20:37:01 +0330

    03-037

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-
+/*
 // 03-036
 let hasDriversLicense = false;
 const passTest = true;
@@ -12,12 +12,26 @@ if (hasDriversLicense) console.log('I can drive!');
 // reserved keywords
 const interface = 'Audio';
 const private = 534;
+*/
 
 
 
+// 03-037
+function logger() {
+    console.log('My name is Poorya');
+}
+
+logger();
 
 
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
 
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
 
 
 
