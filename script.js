8bae08d1c7ac98e6c0729561ecdabdfc4ee2e165
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-04-27 12:04:41 +0330

    02-018

---
+++ script.js
@@ -0,0 +1,114 @@
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
+
+// console.log('Poorya');
+// console.log(38);
+
+
+// let firstName = "PoOrYa";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// let PI = 3.1415;
+
+/*
+let num = 40; //1.number
+console.log("the type of " + num + " is " + typeof (num))
+
+let lastName = "fayazi"; //2.string
+console.log("the type of " + lastName + " is " + typeof (lastName))
+
+let fine = true; //3.boolean
+console.log("the type of " + fine + " is " + typeof (fine))
+
+let unDefined; //4.undefined
+console.log("the type of " + unDefined + " is " + typeof (unDefined))
+
+let tohi = null  //5.empty or Null
+console.log("the type of " + tohi + " is " + typeof (tohi))
+
+let namad = ""; //6.symbol
+console.log("the type of " + namad + " is " + typeof (namad))
+
+let adadBozorg = 9999; //7.BigInt
+console.log("the type of " + adadBozorg + " is " + typeof (adadBozorg))
+*/
+
+// true;
+/*
+console.log(true);
+console.log("true");
+console.log(typeof true)
+*/
+
+
+/*
+const massMark = 78;
+const heightMark = 1.69;
+const massJhon = 92;
+const heightJhon = 1.95;
+
+let BMIMark;
+let BMIJhon;
+let markHigherBMI;
+
+BMIMark = massMark / (heightMark ** 2);
+BMIJhon = massJhon / (heightJhon ** 2);
+markHigherBMI = BMIMark > BMIJhon;
+
+console.log(BMIMark);
+console.log(BMIJhon);
+console.log(markHigherBMI);
+*/
+
+/*
+const firstName = "Poorya";
+const lastName = "Fayazi";
+const age = 38;
+
+const desc = `My name is ${firstName} ${lastName} and my age is ${age}.`;
+console.log(desc);
+console.log(`this is
+how to use backtics
+instead of
+double quotation " "
+or single quot ' '
+!!!`);
+
+console.log("this is \n\
+how to use double quotation \n\
+instead of \n\
+backticks  \n\
+or single quot ' ' \n\
+!!!");
+*/
+
+/*
+const age = 17;
+const isOldEnough = age >= 18;
+
+if (isOldEnough) {     // if (age >= 18) {
+    console.log(`can get driver license 🚕`);
+} else {
+    console.log(`${18 - age} years later can get driver license 😊`);
+}
+*/
+
+const birthYear = 2000;
+if (birthYear % 100 === 0) {
+    console.log(`the birth century is ${Math.floor(birthYear / 100) + 1}`);
+} else {
+    console.log(`the birth century is ${Math.ceil(birthYear / 100)}`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
