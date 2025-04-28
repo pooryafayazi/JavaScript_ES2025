433d440909b26eae0dfdb4592a323e3ddab6274f
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-04-28 16:15:00 +0330

    03-039

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -15,8 +15,9 @@ const private = 534;
 */
 
 
-
+/*
 // 03-037
+// Function Declaration
 function logger() {
     console.log('My name is Poorya');
 }
@@ -24,6 +25,7 @@ function logger() {
 logger();
 
 
+// Function Declaration
 function fruitProcessor(apples, oranges) {
     console.log(apples, oranges);
     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
@@ -32,6 +34,55 @@ function fruitProcessor(apples, oranges) {
 
 const appleJuice = fruitProcessor(5, 0);
 console.log(appleJuice);
+*/
+
+
+
+
+
+// 03-038
+// Function Declaration (called before declaration)
+const age1 = calcAge1(1986); //called before declaration
+
+function calcAge1(birthYear) {
+    return 2025 - birthYear;
+}
+
+console.log(age1);
+
+
+// Function Expression‍
+const calcAge2 = function (birthYear) {
+    return 2025 - birthYear;
+}
+
+const age2 = calcAge2(1986);
+console.log(age2);
+
+
+
+
+// 03-039
+// Arrow Function with Block Body and Implicit Return
+const calcAge3 = birthYear => 2025 - birthYear;
+
+const age3 = calcAge3(1986);
+console.log(age3);
+
+
+// Arrow Function with Block Body and Explicit Return
+const yearsUnitRetriement = (birthYear, firstName) => {
+    const age = 2025 - birthYear;
+    const retirement = 60 - age;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUnitRetriement(1986, 'Poorya'));
+console.log(yearsUnitRetriement(1982, 'Bob'));
+
+
+
+
 
 
 
